bcf2bcce4ff1db3f95c354bd261dbbb107cb65d4
Karan Munjani <karanmunjani0521@gmail.com>
2020-12-22 15:15:21 +0530

    passing Props to component rendered by Route

--- src/App.js
+++ src/App.js
@@ -18,9 +18,18 @@ class App extends Component {
           <Switch>
             //will return first path which matches with url in order of below
             descirbed routes
-            <Route path="/products" component={Products}></Route>
+            <Route
+              path="/products"
+              render={(props) => <Products sortBy="name" {...props}></Products>}
+            ></Route>
             <Route path="/posts" component={Posts}></Route>
             <Route path="/admin" component={Dashboard}></Route>
+            {/* If none of the previous routes render anything,
+            this route acts as a fallback.
+
+            Important: A route with path="/" will *always* match
+            the URL because all URLs begin with a /. So that's
+            why we put this one last of all */}
             <Route path="/" component={Home}></Route>
           </Switch>
         </div>
